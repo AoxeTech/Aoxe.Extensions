183c26d8c2b9363f9699eda8f8c25b93ecac6e50
Mutuduxf <aeondxf@live.com>
2019-09-11 11:19:41 +0800

    Add IsNullOrDefault in NullableExtension

--- Zaabee.Extensions/NullableExtension.cs
+++ Zaabee.Extensions/NullableExtension.cs
@@ -3,8 +3,10 @@ namespace Zaabee.Extensions
     public static class NullableExtension
     {
         public static bool IsNull<T>(this T param) => param == null;
-        
+
         public static bool IsNotNull<T>(this T param) => param != null;
+
+        public static bool IsNullOrDefault<T>(this T param) => param is null || param.Equals(default);
 
         public static T TryGetValue<T>(this T? param) where T : struct => param ?? default;
     }
