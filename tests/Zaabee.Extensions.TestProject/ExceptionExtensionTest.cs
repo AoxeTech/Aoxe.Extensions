ecd250acead59b960f2f5caad9c16108370be575
Mutuduxf <aeondxf@live.com>
2020-03-03 17:45:44 +0800

    Develop

---
+++ tests/Zaabee.Extensions.TestProject/ExceptionExtensionTest.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace Zaabee.Extensions.TestProject
+{
+    public class ExceptionExtensionTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var inmostEx = new Exception("This is the inmost exception.");
+            var ex0 = new Exception("",inmostEx);
+            var ex1 = new Exception("",ex0);
+            Assert.Equal(inmostEx,ex1.GetInmostException());
+            Assert.Equal(inmostEx,inmostEx.GetInmostException());
+        }
+    }
+}
