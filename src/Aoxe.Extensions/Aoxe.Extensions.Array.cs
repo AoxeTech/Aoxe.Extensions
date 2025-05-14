d16c4ef763e17361f6aa968419d4c917134d803e
Mutudu <aeondxf@live.com>
2025-05-14 16:59:52 +0800

    dev

--- src/Aoxe.Extensions/Aoxe.Extensions.Array.cs
+++ src/Aoxe.Extensions/Aoxe.Extensions.Array.cs
@@ -2,25 +2,26 @@ namespace Aoxe.Extensions;
 
 public static partial class AoxeExtension
 {
-    public static Span<T> ToSpan<T>(this T[] array) => new(array);
+    public static Span<T> ToSpan<T>(this T[] array) =>
+        new(array ?? throw new ArgumentNullException(nameof(array)));
 
     public static Span<T> ToSpan<T>(this T[] array, int start, int length) =>
-        new(array, start, length);
+        new(array ?? throw new ArgumentNullException(nameof(array)), start, length);
 
     public static Memory<T> ToMemory<T>(this T[] array) => new(array);
 
     public static Memory<T> ToMemory<T>(this T[] array, int start, int length) =>
-        new(array, start, length);
+        new(array ?? throw new ArgumentNullException(nameof(array)), start, length);
 
     public static ReadOnlySpan<T> ToReadOnlySpan<T>(this T[] array) => new(array);
 
     public static ReadOnlySpan<T> ToReadOnlySpan<T>(this T[] array, int start, int length) =>
-        new(array, start, length);
+        new(array ?? throw new ArgumentNullException(nameof(array)), start, length);
 
     public static ReadOnlyMemory<T> ToReadOnlyMemory<T>(this T[] array) => new(array);
 
     public static ReadOnlyMemory<T> ToReadOnlyMemory<T>(this T[] array, int start, int length) =>
-        new(array, start, length);
+        new(array ?? throw new ArgumentNullException(nameof(array)), start, length);
 
     public static ReadOnlySequence<T> ToReadOnlySequence<T>(this T[] array) => new(array);
 
@@ -28,5 +29,5 @@ public static partial class AoxeExtension
         this T[] array,
         int start,
         int length
-    ) => new(array, start, length);
+    ) => new(array ?? throw new ArgumentNullException(nameof(array)), start, length);
 }
